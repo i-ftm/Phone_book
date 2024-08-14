cea4f291bcf3f552bd118a13a1a5239868ce39c0
FTM <ftm.nateghi@gmail.com>
2024-08-14 21:02:22 +0330

    it's base of the project

--- Phone ]_book.py
+++ Phone ]_book.py
@@ -1,12 +1,21 @@
+import re
 class Phone_book:
-    def __init__(self, name, phone,email):
-        self.name = name
-        self.phone = phone
-        self.email = email
-
-
-    def add_contact(self,*args):
+    def __init__(self):
         pass
+
+    def add_contact(self,name,email,phone):
+        file = open('contacts.txt','a')
+        if len(phone) != 11 :
+            raise ValueError('There is a problem with the phone number')
+        if not (re.match(r'^[A-Za-z0-9_]+@[a-zA-Z]+\.com',email)) :
+            raise ValueError('There is a problem with the email address')
+        file.write(f"""{
+                    'name' : {name},
+                    'email' : {email},
+                    'phone' : {phone},
+                     }
+                    """)
+        file.close()
 
     def update_contact(self,*args):
         pass
