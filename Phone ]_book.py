dde92841c9147b0641ae755253bb9a15e5cb0579
FTM <ftm.nateghi@gmail.com>
2024-08-14 22:21:59 +0330

    In this version, all the methods except the sorting method have been completed

--- Phone ]_book.py
+++ Phone ]_book.py
@@ -1,4 +1,6 @@
 import re
+import os
+
 class Phone_book:
     def __init__(self):
         pass
@@ -13,23 +15,86 @@ class Phone_book:
                     'name' : {name},
                     'email' : {email},
                     'phone' : {phone},
-                     }
+                     }\n
                     """)
         file.close()
 
-    def update_contact(self,*args):
-        pass
+    def update_contact(self,name,email,phone):
+        my_file =open('contacts.txt','r')
+        file = list(my_file)
+        my_file.close()
+        contact = {
+            'name':name,
+            'email':email,
+            'phone':phone,
+        }
 
-    def delete_contact(self,*args):
-        pass
+        if contact in file :
+            file.remove(contact)
+            update_item=input("Which item do you want to edit? for name write 'n' , for email address write 'e' , for phonenumber write 'p'")
+            for i in range(len(update_item)):
+                if update_item[i] == 'n':
+                    contact['name']=input("Enter the name")
+                elif update_item[i] == 'e':
+                    email = input("Enter the email address")
+                    if not (re.match(r'^[A-Za-z0-9_]+@[a-zA-Z]+\.com', )):
+                        raise ValueError('There is a problem with the email address')
+                    contact['email'] = email
+                elif update_item[i] == 'p':
+                    phone = input("Enter the phone number")
+                    if len(phone) != 11:
+                        raise ValueError('There is a problem with the phone number')
+                    contact['phone'] = phone
+                else:
+                    raise ValueError('does not exist')
+                file.append(contact)
+
+                save_item = input("Would you like to save it? if yes write 'y' and if no write 'n' ")
+                if save_item == 'n':
+                    pass
+                elif save_item == 'y':
+                    os.remove('contacts.txt')
+                    new_file=open('contacts.txt','a')  #This file has been updated
+                    for i in range(len(file)):
+                        new_file.write(f'{file[i]}\n')
+                    new_file.close()
+
+                else:
+                    raise ValueError('There is a problem with the email address')
+
+
+    def delete_contact(self,name,email,phone):
+        my_file = open('contacts.txt', 'r')
+        file = list(my_file)
+        my_file.close()
+        contact = {
+            'name': name,
+            'email': email,
+            'phone': phone,
+        }
+
+        if contact in file:
+            file.remove(contact)
+
+        save_item = input("Would you like to save it? if yes write 'y' and if no write 'n' ")
+        if save_item == 'n':
+            pass
+        elif save_item == 'y':
+            os.remove('contacts.txt')
+            new_file = open('contacts.txt', 'a')  # This file has been updated
+            for i in range(len(file)):
+                new_file.write(f'{file[i]}\n')
+            new_file.close()
+
+        else:
+            raise ValueError('There is a problem with the email address')
 
     def show_contacts(self):
-        pass
+        file = open('contacts.txt', 'r')
+        print(file.read())
 
     def sort_contacts(self):
         pass
 
-    def save_contact(self):
-        pass
 
 
