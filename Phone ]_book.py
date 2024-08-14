b60a1c45df49f3b58f59649d172629616b364ff3
FTM <ftm.nateghi@gmail.com>
2024-08-14 20:12:15 +0330

    it's base of the project

---
+++ Phone ]_book.py
@@ -0,0 +1,26 @@
+class Phone_book:
+    def __init__(self, name, phone,email):
+        self.name = name
+        self.phone = phone
+        self.email = email
+
+
+    def add_contact(self,*args):
+        pass
+
+    def update_contact(self,*args):
+        pass
+
+    def delete_contact(self,*args):
+        pass
+
+    def show_contacts(self):
+        pass
+
+    def sort_contacts(self):
+        pass
+
+    def save_contact(self):
+        pass
+
+
